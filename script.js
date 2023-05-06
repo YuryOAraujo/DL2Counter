d554cda406056adde5154c377398b014f6f6914c
YuryOAraujo <127779626+YuryOAraujo@users.noreply.github.com>
2023-05-05 21:37:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const actions = Array.from(document.querySelectorAll('[data-action]'));
+
+let counter = localStorage.getItem("counter") || 0;
+
+document.querySelector(".counter-value").innerText = counter;
+
+if (counter > 1) {
+    document.querySelector(".counter-value").style.color = "#ff0000";
+}
+
+actions.forEach(action => {
+    action.addEventListener('click', () => {
+        const action_name = action.dataset.action;
+
+        switch (action_name) {
+            case 'increase':
+                counter++;
+                localStorage.setItem("counter", counter);
+                break;
+            case 'decrease':
+                if (counter > 0) {
+                    counter--;
+                    localStorage.setItem("counter", counter);
+                } else {
+                    counter = 0;
+                }
+                break;
+            case 'reset':
+                counter = 0;
+                break;
+            case 'save':
+                localStorage.setItem("counter", counter);
+                break;
+            case 'clear':
+                localStorage.removeItem("counter");
+                counter = 0;
+                break;
+        }
+
+        document.querySelector(".counter-value").innerText = counter;
+
+        if (counter > 0) {
+            document.querySelector(".counter-value").style.color = "#ff0000";
+        } else {
+            document.querySelector(".counter-value").style.color = "#ffffff";
+        }
+    });
+});
